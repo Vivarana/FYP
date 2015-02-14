9e01e62907d37998a69d3eb40a8497754355ec66
Mihil Ranathunga <mihilranathunga@gmail.com>
2015-02-14 22:59:02 +0530

    reverted to old version

--- vivarana/static/sunburst/base_custom.js
+++ vivarana/static/sunburst/base_custom.js
@@ -2,28 +2,23 @@ var percentageSliderValue = 0.01;
 
 $(document).ready(function () {
     $.material.init();
-
-    // Slider to display depth
     $("#percentage-slider").noUiSlider({
         start: [10],
         step: 10,
-        range: {
-            'min': [0],
-            'max': [100]
-        }
+	    range: {
+		'min': [0],
+		'max': [100]
+	}
     });
 
-    // depth display slider event handler
-    $("#percentage-slider").on({'slide': function () {
-            percentageSliderValue = $(this).val();
-            $("#percentage-opacity-text").text("Maximum Depth to Display: " + percentageSliderValue + " %");
+    $("#close").on("click",function(){
+            $("#rulegenerator").toggle();
+            $("#chart2").toggle();
+        return false;
+    }
+    );
 
-        }, 'change': function () {
-            updateData();
-        }});
-
-    // event handler for generate rule in nav bar
-    $("#sequence-rule-gen").on("click", function () {
+    $("#sequence-rule-gen").on("click",function(){
         $("#chart2").toggle();
         $("#rulegenerator").toggle();
     });
@@ -37,34 +32,6 @@ $(document).ready(function () {
         $("[data-toggle='tooltip']").tooltip();
     });
 
-    // Token field and suggestions
-    var engine = new Bloodhound({
-        local: $.map(uniqueValues, function (state) {
-            return { value: state };
-        }),
-        datumTokenizer: function (d) {
-            return Bloodhound.tokenizers.whitespace(d.value);
-        },
-        queryTokenizer: Bloodhound.tokenizers.whitespace
-    });
 
-    engine.initialize();
-
-    $('#tokenfield-typeahead').tokenfield({
-        typeahead: [null, { source: engine.ttAdapter() }]
-    });
-
-    // when confirm button was clicked
-    $("#ruleconfirm").click(function () {
-        alert($('#tokenfield-typeahead').tokenfield('getTokens')[1]["value"]);
-    });
-
-    // when close button on rule generator clicked
-    $("#close").on("click", function () {
-            $("#rulegenerator").toggle();
-            $("#chart2").toggle();
-            return false;
-        }
-    );
 });
 
